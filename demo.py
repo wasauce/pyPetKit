debb85bcc91ea93b922f0bcbd8f12f8d111c6008
Ben Horan <benh@geeksforhire.com.au>
2021-08-01 23:39:46 +1000

    Created demo.py script to test / demo module [WIP]

---
+++ demo.py
@@ -0,0 +1,20 @@
+import datetime
+
+from pypetkit import PetKitAPI, schedule
+from demo_settings import (API_USERNAME, API_PASSWORD, API_COUNTRY_CODE, API_LOCALE_CODE, API_TIMEZONE)
+
+from pprint import pprint
+
+petkit_api = PetKitAPI(API_USERNAME, API_PASSWORD, API_COUNTRY_CODE, API_LOCALE_CODE, API_TIMEZONE)
+
+server_info_json = petkit_api.get_api_server_by_country(API_COUNTRY_CODE)
+
+pprint(server_info_json)
+
+petkit_api.get_all_devices()
+
+feeders = petkit_api.get_sensors()
+
+pprint(feeders)
+
+pass
---
+++ demo_settings.py
@@ -0,0 +1,8 @@
+# User Credentials for demo.py
+
+API_USERNAME        = "<<EMAIL>>"
+API_PASSWORD        = "<<PASSWORD>"
+
+API_COUNTRY_CODE    = "AU"
+API_LOCALE_CODE     = "en-AU"
+API_TIMEZONE        = "Australia/Melbourne"
