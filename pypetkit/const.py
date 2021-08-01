46325121390dad7ab3516e2aeed116088370a307
Ben Horan <benh@geeksforhire.com.au>
2021-08-01 23:36:05 +1000

    Added extra HTTP headers and request fields to authentication method.
    Added handling of locale, timezone, country to module (passing such info to the server produces results in the correct
    language and timezone).
    
    Added a new method to obtain a list of API servers from PetKit 'passport.petkt.com' server
        - provides login URL's and authentication methods for each country

--- pypetkit/const.py
+++ pypetkit/const.py
@@ -1,5 +1,6 @@
 
 API_LOGIN_URL = 'http://api.petkt.com/latest/user/login'
 API_DEVICE_URL ='http://api.petkt.com/latest/discovery/device_roster'
+API_SERVER_LIST_URL = 'https://passport.petkt.com/v1/regionservers'
 API_FEEDERMINI_DEVICE_URL = 'http://api.petkt.com/latest/feedermini/owndevices'
 API_FEEDERMINI_HISTORY_URL = 'http://api.petkt.com/latest/feedermini/dailyfeeds'
--- pypetkit/pypetkit.py
+++ pypetkit/pypetkit.py
@@ -2,11 +2,14 @@ import hashlib
 import json
 import logging
 import requests
-from datetime import datetime, timedelta
+import pytz
+
+from datetime import datetime, time, timedelta
 
 from .const import (
     API_LOGIN_URL,
-    API_FEEDERMINI_DEVICE_URL
+    API_FEEDERMINI_DEVICE_URL,
+    API_SERVER_LIST_URL
 )
 from .device import PetKitDevice
 from .history import PetKitHistory
@@ -15,24 +18,27 @@ _LOGGER = logging.getLogger(__name__)
 
 
 class PetKitAPI:
-    def __init__(self, user, password, access_token=None):
+    def __init__(self, user, password, country = "AU", locale = "en-AU", timezone="Australia/Melbourne", access_token=None):
         """ Initialize PetKit API """
         hash = hashlib.md5()
         hash.update(password.encode('utf-8'))
         self._user = user
         self._password = hash.hexdigest()
+        self._country = country
+        self._locale = locale
+        self._tzone = pytz.timezone(timezone)
         self._access_token = access_token
         self._expiration_date = datetime.utcnow()
         self.feeders = {}
 
     @property
     def is_authorized(self):
-        return self._expiration_date < datetime.utcnow()
+        return self._expiration_date > datetime.utcnow()
 
     def get_all_devices(self):
         """Populate all devices in the PetKit account."""
-        if is_authorized() == False:
-            request_token()
+        if self.is_authorized == False:
+            self.request_token()
         
         result = requests.get(
             API_FEEDERMINI_DEVICE_URL,
@@ -50,13 +56,29 @@ class PetKitAPI:
 
     def request_token(self):
         """Request access and refresh tokens from PetKit."""
+        headers = {
+            "User-Agent": "PETKIT/7.26.1 (iPhone; iOS 14.7.1; Scale/3.00)",
+            "X-Timezone": f"{round(self._tzone._utcoffset.seconds/60/60)}.0",
+            "X-Api-Version": "7.26.1",
+            "X-Img-Version": "1",
+            "X-TimezoneId": self._tzone.zone,
+            "X-Client": "ios(14.7.1;iPhone13,4)",
+            "X-Locale": "en_US"
+        }
         params = {
+            "timezoneId": self._tzone.zone,
+            "timezone": f"{round(self._tzone._utcoffset.seconds/60/60)}.0",
             "username": self._user,
             "password": self._password,
+            "locale": self._locale,
+            "source": "app.petkit-ios-oversea",
+            "platform": "ios",
+            "version": "14.7.1",
+            "name": "iPhone13,4",
             "encrypt": 1,
         }
 
-        result = requests.post(API_LOGIN_URL, data=params)
+        result = requests.post(API_LOGIN_URL, data=params, headers=headers)
         try:
             createdAt = datetime.strptime(
                 result.json()['result']['session']['createdAt'],
@@ -75,7 +97,29 @@ class PetKitAPI:
             _LOGGER.error("Error requesting token from PetKit: {}".format(err))
 
     def get_token(self):
-      return self._access_token
+        return self._access_token
     
-    def getSensors(self):
+    def get_sensors(self):
         return self.feeders
+
+    def get_api_server_by_country(self, country="AU"):
+        headers = {
+            "User-Agent": "PETKIT/7.26.1 (iPhone; iOS 14.7.1; Scale/3.00)",
+            "X-Timezone": f"{round(self._tzone._utcoffset.seconds/60/60)}.0",
+            "X-Api-Version": "7.26.1",
+            "X-Img-Version": "1",
+            "X-TimezoneId": self._tzone.zone,
+            "X-Client": "ios(14.7.1;iPhone13,4)",
+            "X-Locale": "en_US"
+        }   
+        try:     
+            result = requests.post(API_SERVER_LIST_URL, headers=headers)
+         
+            result_json = result.json()
+            country_data = next((item for item in result_json['result']['list'] if item["id"] == country), None)
+
+            return country_data
+        except:
+            pass
+
+        return None
